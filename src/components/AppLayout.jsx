51324d0b89684324db5de76af390faaec2101b7a
Orion Gael <orion.gael@gmail.com>
2023-12-05 15:26:51 -0800

    More mobile optimization and added footer

--- src/components/AppLayout.jsx
+++ src/components/AppLayout.jsx
@@ -1,6 +1,7 @@
 import { Outlet, useNavigate } from "react-router-dom";
 import { useUserContext } from "../UserContext";
 import Header from "./ui/Header";
+import Footer from "./ui/Footer";
 
 function AppLayout() {
   const { data } = useUserContext();
@@ -9,6 +10,7 @@ function AppLayout() {
     <div className="app-body">
       <Header name={data?.sessionData?.user_metadata.name} />
       <Outlet />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/ui/Footer.jsx
@@ -0,0 +1,5 @@
+function Footer() {
+  return <footer>© 2023 Orion Gael - All rights reserved</footer>;
+}
+
+export default Footer;
