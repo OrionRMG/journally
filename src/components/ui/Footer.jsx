086361b15aa536a5dfb69a43380824c4238baf3f
Orion Gael <orion.gael@gmail.com>
2023-12-05 15:35:07 -0800

    Fixed username placement issue 2, and changed footer text

--- src/components/ui/Footer.jsx
+++ src/components/ui/Footer.jsx
@@ -1,5 +1,5 @@
 function Footer() {
-  return <footer>© 2023 Orion Gael - All rights reserved</footer>;
+  return <footer>© 2023 Journally - All rights reserved</footer>;
 }
 
 export default Footer;
